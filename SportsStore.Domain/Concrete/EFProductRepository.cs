4f6a348cb73286bb49b2b8edb3ed1a0bb381f4bc
trinakriae <trinakriae@gmail.com>
2018-02-15 19:22:15 +0100

    Added IProductRepository implementation EFProductRepository Changed accessibility of EFDbContext to private

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext _context;
+
+        public EFProductRepository()
+        {
+            _context = new EFDbContext();
+        }
+
+        public EFProductRepository(EFDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Product> Products
+        {
+            get { return _context.Products; }
+        }
+    }
+}
