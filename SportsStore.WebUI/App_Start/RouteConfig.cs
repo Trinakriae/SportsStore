6475151d92978380cabd24daab35b80d66b29ba9
trinakriae <trinakriae@gmail.com>
2018-02-19 19:37:25 +0100

    Added new routes for category parameter in List action method

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,15 +14,34 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                null, // Only matches the empty URL (i.e. /)
                 null,
-                url: "Page{page}",
-                defaults: new { controller = "Product", action = "List" }
+                defaults: new { controller = "Product", action = "List", category = (string)null, page = 1 }
             );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                null,
+                url: "Page{page}",
+                defaults: new { controller = "Product", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" } //page must be numerical
+            );
+
+            routes.MapRoute(
+               null,
+               url: "{category}",
+               defaults: new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(
+                null,
+                url: "{category}/Page{page}",
+                defaults: new { controller = "Product", action = "List" },
+                constraints: new { page = @"\d+" } //page must be numerical
+            );
+
+            routes.MapRoute(
+                null,
+                url: "{controller}/{action}"
             );
         }
     }
