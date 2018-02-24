309042c305a25541be45e3b84fda1a7bbaf97c71
trinakriae <trinakriae@gmail.com>
2018-02-24 16:30:06 +0100

    Added Admin index view, added to Product domain class the displayname for ProductID

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [DisplayName("ID")]
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
